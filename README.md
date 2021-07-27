56aa02618d641958a61039e024ddfd2887d91db7
Miron Jakubowski <mijakubowski@gmail.com>
2021-07-27 19:08:51 +0200

    Initialize React app

---
+++ README.md
@@ -0,0 +1,1 @@
+# Supabase demo app
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./App.module.scss";
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <p>May the Force be with You</p>
+    </div>
+  );
+}
+
+export default App;
